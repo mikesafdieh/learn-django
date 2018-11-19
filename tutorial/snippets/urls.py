17aafd65a4fb309da18cdfee8b2e0d9c3050b5d7
Michael Safdieh <mikesafdieh@gmail.com>
2018-11-19 11:58:26 -0500

    update views and urls; wrap views with @api_view; add httpie notes

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets import views
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+# for extending our API endpoints to include the content type 
+# ex: "http://example.com/api/items/4.json" - ('.json' here is the content type)
+urlpatterns = format_suffix_patterns(urlpatterns)
